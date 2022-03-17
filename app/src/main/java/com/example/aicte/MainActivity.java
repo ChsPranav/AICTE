579e6a64d8310727fa44af1d5a90ade9201fad09
seshapranav <seshapranav@gmail.com>
2022-03-17 21:21:47 +0530

    first page changed

--- app/src/main/java/com/example/aicte/MainActivity.java
+++ app/src/main/java/com/example/aicte/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,14 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView img = (ImageView)findViewById(R.id.imageView);
-
-        img.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+                finish();
             }
-        });
+        }, 5000);
+//        img.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View v) {
+//                Intent intent = new Intent();
+//                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+//
+//            }
+//        });
     }
 }
