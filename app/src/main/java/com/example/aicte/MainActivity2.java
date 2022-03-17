ea72363484b5217f8303f34e39ca2dd6dde6fb6f
18mis7023 <hemanth.18mis7023@vitap.ac.in>
2022-03-17 23:35:00 +0530

    xml completed

--- app/src/main/java/com/example/aicte/MainActivity2.java
+++ app/src/main/java/com/example/aicte/MainActivity2.java
@@ -8,29 +8,32 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
-    private CardView card1,card2,card3,card4;
+    private CardView card1,card2,card3,card4,card5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         card1=(CardView) findViewById(R.id.card1);
         card2=(CardView) findViewById(R.id.card2);
-//        card3=(CardView) findViewById(R.id.card3);
-//        card4=(CardView) findViewById(R.id.card4);
+        card3=(CardView) findViewById(R.id.card3);
+        card4=(CardView) findViewById(R.id.card4);
+        card5=(CardView) findViewById(R.id.card5);
         card1.setOnClickListener(this);
         card2.setOnClickListener(this);
-//        card3.setOnClickListener(this);
-//        card4.setOnClickListener(this);
+        card3.setOnClickListener(this);
+        card4.setOnClickListener(this);
+        card5.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent i;
+        Intent i=new Intent(this,MainCard1.class);
         switch (v.getId()){
-            case R.id.card1: i=new Intent(this,MainCard1.class);startActivity(i);break;
-            case R.id.card2: i=new Intent(this,MainCard2.class);startActivity(i);break;
-//            case R.id.card3: i=new Intent(this,MainCard3.class);startActivity(i);break;
-//            case R.id.card4: i=new Intent(this,MainActivity4.class);startActivity(i);break;
+            case R.id.card1: i.putExtra("ID","college");startActivity(i);break;
+            case R.id.card2: i.putExtra("ID","id2");startActivity(i);break;
+            case R.id.card3: i.putExtra("ID","id3");startActivity(i);break;
+            case R.id.card4: i.putExtra("ID","id4");startActivity(i);break;
+            case R.id.card5: i.putExtra("ID","id5");startActivity(i);break;
             default: break;
         }
     }
